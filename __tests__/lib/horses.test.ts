8213456ce7ce2dc23952774dbb4b4489919571d6
Trevor Frizzell <trevor@frizzell.ca>
2023-02-04 15:28:36 -0400

    Added an extra test horse

--- __tests__/lib/horses.test.ts
+++ __tests__/lib/horses.test.ts
@@ -41,10 +41,12 @@ describe(`calculateStudFee`, (): void => {
     test(`returns the expected values`, async (): Promise<void> => {
         const values: StudFeeTestData[] = [
             [StudFeeFormula.Apex, 14, 82000],
+            [StudFeeFormula.Apex, 10474, 72000],
             [StudFeeFormula.Apex, 26326, 26000],
             [StudFeeFormula.Ridge, 14, 170000],
+            [StudFeeFormula.Ridge, 10474, 150000],
             [StudFeeFormula.Ridge, 26326, 109000],
-        ]
+        ];
 
         for (const [formula, id, expected] of values)
             expect(await calculateStudFee({ id, formula })).toEqual(expected);
@@ -63,6 +65,28 @@ describe(`getHorse`, (): void => {
     test(`returns the expected values`, async (): Promise<void> => {
         const values: HorseTestData[] = [
             [14, { id: 14, name: 'Astronomical', sireId: null, damId: null, retired: true }],
+            [10474, { id: 10474, name: 'Readly Express', sireId: null, damId: null, retired: true }],
+            [26326, { id: 26326, name: 'Leader Apex', sireId: 15729, damId: 186, retired: false }],
+        ]
+
+        for (const [id, expected] of values)
+            expect(await getHorse(id)).toEqual(expected);
+    });
+});
+
+describe(`getHorse`, (): void => {
+    test(`exists`, (): void => {
+        expect(getHorse).not.toBeUndefined();
+    });
+
+    test(`is a function`, (): void => {
+        expect(typeof getHorse).toEqual('function');
+    });
+
+    test(`returns the expected values`, async (): Promise<void> => {
+        const values: HorseTestData[] = [
+            [14, { id: 14, name: 'Astronomical', sireId: null, damId: null, retired: true }],
+            [10474, { id: 10474, name: 'Readly Express', sireId: null, damId: null, retired: true }],
             [26326, { id: 26326, name: 'Leader Apex', sireId: 15729, damId: 186, retired: false }],
         ]
 
